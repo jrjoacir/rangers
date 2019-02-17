a13917bb16424cfae295c9c66a4466f8de9641d0
Joacir Junior <jr.joacir@gmail.com>
2019-02-17 17:03:24 -0300

    Create migration for create Set table

---
+++ db/migrations/002_create_set_table.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+    create_table(:set) do
+      primary_key :id
+      String :name, null: false, unique: true
+      String :description
+    end
+  end
+
+  down do
+    drop_table(:set)
+  end
+end
